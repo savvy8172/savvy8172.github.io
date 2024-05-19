b136553d890bf4ac956c9c189d587bbfc87d1929
savvy8172 <s3953544@student.rmit.edu.au>
2024-05-20 00:12:21 +1000

    l

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -26,14 +26,62 @@ function toggleMute() {
   if (myVideo.muted) {
       myVideo.muted = false;
       muteUnmuteButton.textContent = 'Mute';
+      muteUnmuteButton.style.backgroundColor = "#d5cea3";
   } else {
       myVideo.muted = true;
       muteUnmuteButton.textContent = 'Unmute';
+      muteUnmuteButton.style.backgroundColor = "#7b775e";
   }
 }
+const increaseVolumeButton = document.querySelector("#increase-volume-btn");
+// Event listener to increase volume on clicking the button
+increaseVolumeButton.addEventListener("click", increaseVolume);
+
+const decreaseVolumeButton = document.querySelector("#decrease-volume-btn");
+// Event listener to decrease volume on clicking the button
+decreaseVolumeButton.addEventListener("click", decreaseVolume);
+
+const loopButton = document.querySelector("#loop-btn");
+// Event listener to loop or replay the video on clicking the button
+loopButton.addEventListener("click", loopVideo);
 
 // Adding event listener to the mute button
-document.addEventListener('DOMContentLoaded', function() {
-  const muteUnmuteButton = document.getElementById('muteButton');
+  const muteUnmuteButton = document.getElementById('muteUnmuteButton');
   muteUnmuteButton.addEventListener('click', toggleMute);
-});
+
+  function replay() {
+    console.log("loop is", loop);
+    if (loop) {
+      myVideo.currentTime = 0;
+      myVideo.play();
+    }
+  }
+  
+  //this function will set the value of loop to true or false
+  function loopVideo() {
+    if (loop) {
+      loop = false;
+      loopButton.style.backgroundColor = "#d5cea3";
+    } else {
+      loop = true;
+      loopButton.style.backgroundColor = "#7b775e";
+    }
+    console.log("loop is", loop);
+  }
+  function updateVolume() {
+    const volume = myVideo.volume;
+    console.log("Volume changed:", volume);
+  }
+  
+  //volume values range from 0 to 1 with an increment of 0.1
+  function increaseVolume() {
+    if (myVideo.volume < 0.9) {
+      myVideo.volume += 0.1;
+    }
+  }
+  
+  //since you do not want the value to go to negative the check is not at 0 but 0.11
+  function decreaseVolume() {
+    if (myVideo.volume > 0.11) {
+      myVideo.volume -= 0.1;
+    }
